dcf48bf2fcbafe88707bf4538905994c62165bac
Peter <99065921@mydavinci.nl>
2021-06-14 12:57:56 +0200

    Typo

--- view/home/index.php
+++ view/home/index.php
@@ -1,4 +1,4 @@
 <div class="container">
  <h1 class="display-1">Welkom bij de Manege.</h1>
- <p class="lead">Op deze website kan u uwzelf registreren en paarden reserveren. Ook kan u nieuwe paarden en ponies aanmelden.</p>
+ <p class="lead">Op deze website kan u uzelf registreren en paarden reserveren. Ook kan u nieuwe paarden en ponies aanmelden.</p>
 </div>
