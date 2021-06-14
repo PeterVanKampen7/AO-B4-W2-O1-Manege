8dec2371e01b13ee0c7092a7bce8dbf56752631d
Peter <99065921@mydavinci.nl>
2021-06-14 10:45:40 +0200

    Working header links and footer

--- controller/HomeController.php
+++ controller/HomeController.php
@@ -1,6 +1,17 @@
 <?php
 
-function index()
-{
+function index(){
 	render("home/index");	
+}
+
+function user(){
+	render("home/user");
+}
+
+function horse(){
+	render("home/horse");
+}
+
+function reservation(){
+	render("home/reservation");
 }
--- view/templates/footer.php
+++ view/templates/footer.php
@@ -1,3 +1,7 @@
-
+<footer class="page-footer fixed-bottom bg-dark text-light">
+  <div class="footer-copyright text-center py-3">
+    © 2021 Peter van Kampen
+  </div>
+</footer>
 </body>
 </html>
